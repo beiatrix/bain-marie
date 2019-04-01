5ee5fba60299ff8f08c2315627798569b7ccda6e
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-31 21:01:57 -0400

    todos api route connected to database - sends real data

--- server/api/todos.js
+++ server/api/todos.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const {Todos} = require('../db/models')
 
 /* GET /api/todos */
-router.get('/', (req, res, next) => {
-  res.send('API is working')
+router.get('/', async (req, res, next) => {
+  // res.send('API is working')
+  try {
+    const todos = await Todos.findAll()
+    res.json(todos)
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 module.exports = router
