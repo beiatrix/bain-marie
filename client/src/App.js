76aa7680a5d6cc27a82ec2f46ccfeefaac45838a
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-31 21:06:31 -0400

    first fetch request from react

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: []
+    }
+  }
+
+  async getTodos() {
+    const res = await fetch('http://localhost:9000/api/todos')
+    console.log(res)
+  }
+
+  componentWillMount() {
+    this.getTodos()
+  }
+
   render() {
     return (
       <div className="App">
